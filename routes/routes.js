a1b3f0613a9eb9787195f117895e83ed621a2f0c
Khush Patel <202001127@daiict.ac.in>
2023-06-25 23:15:27 +0530

    Just small change

--- routes/routes.js
+++ routes/routes.js
@@ -66,6 +66,7 @@ router.get('/getOne/:id', async (req, res) => {
     try{
         const data = await ErDiagram.findById(id);
         res.status(200).json(data)
+        console.log(data);
     }
     catch(error){
         res.status(500).json({message: error.message})
